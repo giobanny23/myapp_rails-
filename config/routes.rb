5a9f2bfd7ea02ac6be5aed5e41a07aa36d130f84
edgar_2_62@hotmail.com <edgar_2_62@hotmail.com>
2017-08-19 23:46:56 -0500

    generate rails_myapp

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'redes_sociales/facebook'
+
+  get 'redes_sociales/twitter'
+
+  get 'redes_sociales/google_mas'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
